a2378f84009789dde5d43458fdb42627de056d7d
magerngulik <magerngulik@gmail.com>
2023-01-04 13:49:57 +0700

    local storage finish

---
+++ lib/bloc/product/product_state.dart
@@ -0,0 +1,43 @@
+part of 'product_bloc.dart';
+
+// ignore: must_be_immutable
+class ProductState extends Equatable {
+  ProductState({
+    this.listData = const <Product>[],
+    this.listSelected = const <Product>[],
+    this.listCart = const <CartModel>[],
+  });
+  List<Product> listData;
+  List<Product> listSelected;
+  List<CartModel> listCart;
+
+  @override
+  List<Object> get props => [listData, listSelected, listCart];
+  Map<String, dynamic> toMap() {
+    return {
+      "product": listData.map((e) => e.toMap()).toList(),
+      "selected": listSelected.map((e) => e.toMap()).toList(),
+      "cart": listCart.map((e) => e.toMap()).toList(),
+    };
+  }
+
+  factory ProductState.fromMap(Map<String, dynamic> map) {
+    return ProductState(
+      listData: List<Product>.from(
+        (map['product'])?.map<Product>(
+          (x) => Product.fromMap(x),
+        ),
+      ),
+      listSelected: List<Product>.from(
+        (map['selected'])?.map<Product>(
+          (x) => Product.fromMap(x),
+        ),
+      ),
+      listCart: List<CartModel>.from(
+        (map['cart'])?.map<CartModel>(
+          (x) => CartModel.fromMap(x),
+        ),
+      ),
+    );
+  }
+}
